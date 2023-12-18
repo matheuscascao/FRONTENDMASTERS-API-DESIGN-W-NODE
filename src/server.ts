7d82c556dbc009bd64efaa13c0d9544f9154ec88
Cascao <matheusfranco23@hotmail.com>
2023-12-18 00:21:32 -0300

    signIn and create user

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,28 @@
 import express from 'express';
+import morgan from 'morgan';
+import router from './router';
+import { protect } from './modules/auth';
+import { createNewUser, signin } from './handlers/user';
 
 const app = express();
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-    console.log("get at / reached");
-    res.status(200);
-    res.json({message: 'hello'});
+app.use((req, res, next) => {
+  req.shhh_secret = 'doggy';
+  next();
 })
 
-export default app;
+app.get('/', (req, res) => {
+  console.log("get at / reached");
+  res.status(200);
+  res.json({ message: 'hello' });
+})
+
+app.use('/api', protect, router);
+
+app.post('/user', createNewUser);
+app.post('/signin', signin);
+
+export default app; 
